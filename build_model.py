fa6f00961a3eccfae5706573ccae119116155648
Koushul <kor11@pitt.edu>
2021-12-22 17:14:45 -0500

    Added HPV network generation

--- build_model.py
+++ build_model.py
@@ -15,18 +15,14 @@ import pandas as pd
 import networkx as nx
 import time
 
-
 tic = time.perf_counter()
 
 sys.path.append("../EM-sCGGM/")
 from em_scggm import em_scggm
 
-# cancer = sys.argv[1]
-cancer = 'MESO'
-# cancer = 'HNSC2'
 
-# root_dir = Path(f'/ihome/hosmanbeyoglu/kor11/tools/PerturbNet/TCGA/{cancer}')
-root_dir = Path(f'/ihome/hosmanbeyoglu/kor11/tools/PerturbNet/Viral/{cancer}')
+cancer = 'HNSC'
+root_dir = Path(f'/ihome/hosmanbeyoglu/kor11/tools/PerturbNet/TCGA/{cancer}')
 scripts = Path(f'/ihome/hosmanbeyoglu/kor11/tools/PerturbNet/scripts')
 
 Z = np.loadtxt(root_dir / 'traits.txt', delimiter='\t')
@@ -34,11 +30,11 @@ Y = np.loadtxt(root_dir / 'expression.txt', delimiter='\t')
 X = np.loadtxt(root_dir / 'genotype.txt', delimiter='\t')
 
 
-lambdaLambda_z = 0.8
-lambdaTheta_yz = 0.02
+lambdaLambda_z = 0
+lambdaTheta_yz = 0.01
 
-lambdaLambda_y = 0.7 
-lambdaTheta_xy = 0.02
+lambdaLambda_y = 0.65
+lambdaTheta_xy = 0.0787
 
 
 print(lambdaLambda_z, lambdaTheta_yz, lambdaLambda_y, lambdaTheta_xy)
@@ -46,7 +42,7 @@ print(lambdaLambda_z, lambdaTheta_yz, lambdaLambda_y, lambdaTheta_xy)
 (estLambda_z, estTheta_yz, estLambda_y, estTheta_xy, estStats) = em_scggm(
     Z, Y, X, 
     lambdaLambda_z, 
-    lambdaTheta_yz, 
+    lambdaTheta_yz,
     lambdaLambda_y, 
     lambdaTheta_xy, 
     max_em_iters = 5, 
@@ -93,56 +89,120 @@ os.chdir(root_dir)
 # plt.savefig('lambda_z.png', dpi=300)
 
 
-traits = pd.read_pickle(root_dir / 'traits_data.pkl')
-mutations = pd.read_pickle(root_dir / 'mutations_data.pkl')
-rnaseq = pd.read_pickle(root_dir / 'rnaseq_data.pkl')
+shutil.copy(scripts / f'{cancer}.log', root_dir)
 
-traits = pd.read_csv(root_dir / 'traits_data.csv').set_index('Unnamed: 0')
-mutations = pd.read_csv(root_dir / 'mutations_data.csv').set_index('Unnamed: 0')
-rnaseq = pd.read_csv(root_dir / 'rnaseq_data.csv').set_index('Unnamed: 0')
+# ph.push_note('CRC', f'EM-sCGGM completed for {cancer} in {round(t, 2)} minutes.')
 
-G = nx.from_scipy_sparse_matrix(estLambda_y)
-G = nx.relabel_nodes(G, dict(zip(range(0, len(rnaseq)), rnaseq.index)))
-neighbor_count = [len(list(nx.neighbors(G, gene))) for gene in rnaseq.index]
-neighbors = pd.DataFrame(neighbor_count, index=rnaseq.index, columns = ['neighbors'])
-G.remove_nodes_from([i for i in neighbors[neighbors.neighbors < 2].index])
-
-print(f'\nGene expression network has {len(G.nodes())} nodes\n')
 
 from scipy.sparse.linalg import inv
 import warnings
 from scipy import sparse
-import matplotlib.pyplot as plt
-CB91_Blue = '#2CBDFE'
-CB91_Green = '#47DBCD'
-CB91_Pink = '#F3A0F2'
-CB91_Purple = '#9D2EC5'
-CB91_Violet = '#661D98'
-CB91_Amber = '#F5B14C'
-Black = '#000000'
 
-color_list = [CB91_Blue, CB91_Pink, CB91_Green, CB91_Amber,
-              CB91_Purple, CB91_Violet, Black]
-plt.rcParams['axes.prop_cycle'] = plt.cycler(color=color_list)
-plt.style.use('ggplot')
+warnings.filterwarnings('ignore')
+outputdir = Path(f'/ihome/hosmanbeyoglu/kor11/tools/PerturbNet/TCGA/{cancer}')
+scripts = Path(f'/ihome/hosmanbeyoglu/kor11/tools/PerturbNet/scripts')
 
-Sigma_z = inv(estLambda_z)
-Sigma_y = sparse.csr_matrix(np.linalg.inv(estLambda_y.tocsr().todense()))
-B_xy = -estTheta_xy * Sigma_y # Indirect mutation perturbation effects on gene expression levels
-B_yz = -estTheta_yz * Sigma_z # Indirect effects of gene expression levels on clinical phenotypes
+
+Theta_xy = ssp.load_npz(outputdir / 'Theta_xy.npz') #Mutations-to-Expression
+Lambda_y = ssp.load_npz(outputdir / 'Lambda_y.npz') #Expression Network
+Lambda_z = ssp.load_npz(outputdir / 'Lambda_z.npz') #Traits Network
+Theta_yz = ssp.load_npz(outputdir / 'Theta_yz.npz') #Expression-to-Traits
+
+
+
+traits = pd.read_csv(outputdir / 'traits_data.csv').set_index('Unnamed: 0')
+mutations = pd.read_csv(outputdir / 'mutations_data.csv').set_index('Unnamed: 0')
+rnaseq = pd.read_csv(outputdir / 'rnaseq_data.csv').set_index('Unnamed: 0')
+traits.index.name = None
+mutations.index.name = None
+rnaseq.index.name = None
+mutations = mutations.T
+
+special_genes = """
+RELA
+PBX1
+SPI1
+HIVEP1
+MXI1
+TBX19
+DNMT1
+SMARCC1
+ZNF410
+HMBOX1
+TFEB
+HINFP
+NFYA
+BPTF
+CREB1
+AR
+STAT6
+TERT
+""".split()
+
+
+df = pd.DataFrame(Theta_yz.tocsr().todense())
+hpv_connected_genes = rnaseq.reset_index().loc[df[df[0] != 0][0].index]['index'].values
+
+print(f'HPV connected genes: {len(hpv_connected_genes)}')
+
+G = nx.from_scipy_sparse_matrix(Lambda_y)
+G = nx.relabel_nodes(G, dict(zip(range(0, len(rnaseq)), rnaseq.index)))
+
+# neighbor_count = [len(list(nx.neighbors(G, gene))) for gene in rnaseq.index]
+# neighbors = pd.DataFrame(neighbor_count, index=rnaseq.index, columns = ['neighbors'])
+
+# cull = []
+# for i in neighbors[neighbors.neighbors < 2].index:
+#     if i not in hpv_connected_genes:
+#         cull.append(i)
+# G.remove_nodes_from(cull)
+
+G.remove_edges_from(nx.selfloop_edges(G))
+G.remove_nodes_from(set(nx.isolates(G)))
+
+print()
+print([i for i in special_genes if i in G.nodes()])
+print()
+
+all_paths = []
+for gene in [i for i in special_genes if i in G.nodes()]:
+    for source in set(G.nodes()) & set(hpv_connected_genes):
+        for path in nx.all_simple_paths(G, source=source, target=gene, cutoff=4):
+            all_paths.append(path)
+
+if len(all_paths) > 0:
+    paths_df = pd.DataFrame(all_paths)
+    print(pd.DataFrame([[paths_df[paths_df[i]==x].__len__() for i in range(4)] for x in [i for i in special_genes if i in G.nodes()]], 
+                index=[i for i in special_genes if i in G.nodes()]))
+
+print()
+print(f'\nGene expression network has {len(G.nodes())} nodes\n')
+
+
+Sigma_z = inv(Lambda_z)
+Sigma_y = sparse.csr_matrix(np.linalg.inv(Lambda_y.tocsr().todense()))
+B_xy = -Theta_xy * Sigma_y # Indirect mutation perturbation effects on gene expression levels
+B_yz = -Theta_yz * Sigma_z # Indirect effects of gene expression levels on clinical phenotypes
 B_xz = B_xy * B_yz # mutation effects on clinical phenotypes
 
-
-impact = pd.DataFrame(B_xz.todense(), columns=traits.columns, index=mutations.columns)
+if len(traits.columns) > 1:
+    impact = pd.DataFrame(B_xz.todense(), columns=traits.columns, index=mutations.index)
+else:
+    impact = pd.DataFrame(B_xz, columns=traits.columns, index=mutations.index)
+impact = impact.abs()
 impact['overall'] = impact.sum(1)
-impact['frequency'] = impact.index.map(lambda x: mutations[x].sum())
 
-impact.sort_values(by='Survival', ascending=True)[:50][::-1].drop(['overall', 'frequency'], axis=1).plot(kind='barh', stacked=True, figsize=(13, 9))
-plt.grid(False)
-plt.tight_layout()
-plt.savefig('top_mutations.png', dpi = 120)
-plt.show()
+# print(impact)
 
+print(impact[impact.overall > 0].sort_values(by='overall', ascending=False)[:20].index)
+check_impact = ['TP53', 'TTN', 'FAT1', 'NOTCH1', 'PIK3CA', 'CASP8', 'NSD1']
 
-shutil.copy(scripts / f'{cancer}.log', root_dir)
-os.system('zip meso.zip MESO.log traits_data.csv mutations_data.csv rnaseq_data.csv Lambda_z.npz Theta_yz.npz Lambda_y.npz Theta_xy.npz traits_data.pkl mutations_data.pkl rnaseq_data.pkl params.json')
+print()
+Theta_df = pd.DataFrame(abs(Theta_xy.tocsr()).sum(1), mutations.index, columns=['weight'])
+Theta_df['frequency'] = Theta_df.index.map(lambda x: mutations.loc[x].sum())
+print(Theta_df.sort_values(by='weight', ascending=False)[:20].index)
+
+print()
+check_impact = [i in impact[impact.overall > 0].sort_values(by='overall', ascending=False)[:25].index for i in ['TP53', 'TTN', 'FAT1', 'NOTCH1', 'PIK3CA', 'CASP8', 'NSD1']]
+check_theta = [i in Theta_df.sort_values(by='weight', ascending=False)[:10].index for i in ['TP53', 'TTN', 'FAT1', 'NOTCH1', 'PIK3CA', 'CASP8', 'NSD1']]
+print(pd.DataFrame([check_impact, check_theta], columns = ['TP53', 'TTN', 'FAT1', 'NOTCH1', 'PIK3CA', 'CASP8', 'NSD1'], index=['in_impact', 'in_theta']).T)
